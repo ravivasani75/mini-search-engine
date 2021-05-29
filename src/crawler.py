e0b36898341c14daad9f104325cf3c700c203171
Ravi Vasani <ravivasani75@gmail.com>
2021-05-29 15:50:48 +0530

    Enhanced crawler with error handling, logging, rate limiting, content deduplication, and data cleaning

--- src/crawler.py
+++ src/crawler.py
@@ -1,18 +1,23 @@
+import logging
 import requests
 from bs4 import BeautifulSoup
 import json
 import os
+import time
+import hashlib
+
+# Setup logging
+logging.basicConfig(filename='crawler.log', level=logging.INFO, 
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 def fetch_content(url):
     try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            return response.text
-        else:
-            print(f"Failed to retrieve {url} with status code {response.status_code}")
+        response = requests.get(url, timeout=10)  # Added timeout
+        response.raise_for_status()  # Raise an HTTPError for bad responses
+        return response.text
     except requests.exceptions.RequestException as e:
-        print(f"Request failed: {e}")
-    return None
+        logging.error(f"Failed to retrieve {url}: {e}")
+        return None
 
 def parse_html(html_content):
     soup = BeautifulSoup(html_content, 'html.parser')
@@ -21,6 +26,13 @@ def parse_html(html_content):
 def save_to_file(data, filename):
     with open(filename, 'w') as f:
         json.dump(data, f, indent=4)
+
+def is_duplicate_content(content, seen_hashes):
+    content_hash = hashlib.md5(content.encode('utf-8')).hexdigest()
+    if content_hash in seen_hashes:
+        return True
+    seen_hashes.add(content_hash)
+    return False
 
 if __name__ == "__main__":
     # Create data directory if it doesn't exist
@@ -31,11 +43,23 @@ if __name__ == "__main__":
     with open('src/websites.txt', 'r') as file:
         urls = file.read().splitlines()
 
-    # Crawl each URL, parse content, and save to file
+    # Keep track of seen URLs and hashes to avoid duplicates
+    seen_urls = set()
+    seen_hashes = set()
+
     for url in urls:
+        if url in seen_urls:
+            logging.info(f"Skipping duplicate URL: {url}")
+            continue
+        seen_urls.add(url)
+
         content = fetch_content(url)
         if content:
-            text = parse_html(content)
+            text = ' '.join(parse_html(content).split())  # Cleaned content
+            if is_duplicate_content(text, seen_hashes):
+                logging.info(f"Skipping duplicate content from {url}")
+                continue
             filename = f"data/{url.replace('https://', '').replace('http://', '').replace('/', '_')}.json"
             save_to_file({"url": url, "content": text}, filename)
-            print(f"Saved content from {url} to {filename}")
+            logging.info(f"Saved content from {url} to {filename}")
+        time.sleep(1)  # Delay to respect server resources
