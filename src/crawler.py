57f3311bccfd525b08e167ccc7521efa3113c146
Ravi Vasani <ravivasani75@gmail.com>
2021-07-07 18:36:24 +0530

    Implemented threading and depth control for the crawler, ensuring accurate and efficient crawling.

--- src/crawler.py
+++ src/crawler.py
@@ -3,8 +3,8 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import os
-import time
-import hashlib
+from concurrent.futures import ThreadPoolExecutor
+from urllib.parse import urljoin, urlparse
 
 # Setup logging
 logging.basicConfig(
@@ -13,61 +13,73 @@ logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
 
-
 def fetch_content(url):
     try:
-        response = requests.get(url, timeout=10)  # Added timeout
-        response.raise_for_status()  # Raise an HTTPError for bad responses
+        response = requests.get(url, timeout=10)
+        response.raise_for_status()
         return response.text
     except requests.exceptions.RequestException as e:
         logging.error(f"Failed to retrieve {url}: {e}")
         return None
 
-
 def parse_html(html_content):
     soup = BeautifulSoup(html_content, "html.parser")
-    return soup.get_text()
+    return soup
 
+def extract_links(soup, base_url):
+    links = set()
+    for link in soup.find_all("a", href=True):
+        href = link.get("href")
+        full_url = urljoin(base_url, href)
+        if urlparse(full_url).scheme in ["http", "https"]:
+            links.add(full_url)
+    return links
 
 def save_to_file(data, filename):
     with open(filename, "w") as f:
         json.dump(data, f, indent=4)
 
+def crawl(url, depth, max_depth, seen_urls):
+    if depth > max_depth:
+        logging.info(f"Maximum depth reached at {url}. Stopping crawl.")
+        return set()
 
-def is_duplicate_content(content, seen_hashes):
-    content_hash = hashlib.md5(content.encode("utf-8")).hexdigest()
-    if content_hash in seen_hashes:
-        return True
-    seen_hashes.add(content_hash)
-    return False
+    if url in seen_urls:
+        logging.info(f"Skipping duplicate URL: {url}")
+        return set()
+    seen_urls.add(url)
 
+    content = fetch_content(url)
+    if content:
+        soup = parse_html(content)
+        text = " ".join(soup.get_text().split())
+        filename = f"data/{url.replace('https://', '').replace('http://', '').replace('/', '_')}.json"
+        save_to_file({"url": url, "content": text}, filename)
+        logging.info(f"Successfully crawled and saved content from {url}")
+
+        if depth < max_depth:
+            links = extract_links(soup, url)
+            return links  # Return links to be crawled at the next depth level
+    return set()
 
 if __name__ == "__main__":
-    # Create data directory if it doesn't exist
     if not os.path.exists("data"):
         os.makedirs("data")
 
-    # Read URLs from websites.txt
     with open("src/websites.txt", "r") as file:
         urls = file.read().splitlines()
 
-    # Keep track of seen URLs and hashes to avoid duplicates
+    max_depth = 2  # Set the maximum crawl depth
     seen_urls = set()
-    seen_hashes = set()
 
-    for url in urls:
-        if url in seen_urls:
-            logging.info(f"Skipping duplicate URL: {url}")
-            continue
-        seen_urls.add(url)
-
-        content = fetch_content(url)
-        if content:
-            text = " ".join(parse_html(content).split())  # Cleaned content
-            if is_duplicate_content(text, seen_hashes):
-                logging.info(f"Skipping duplicate content from {url}")
-                continue
-            filename = f"data/{url.replace('https://', '').replace('http://', '').replace('/', '_')}.json"
-            save_to_file({"url": url, "content": text}, filename)
-            logging.info(f"Saved content from {url} to {filename}")
-        time.sleep(1)  # Delay to respect server resources
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        future_to_url = {executor.submit(crawl, url, 0, max_depth, seen_urls): url for url in urls}
+        for depth in range(1, max_depth + 1):
+            next_level_urls = set()
+            for future in future_to_url:
+                result = future.result()
+                if result:
+                    next_level_urls.update(result)
+            if not next_level_urls:
+                break
+            future_to_url = {executor.submit(crawl, url, depth, max_depth, seen_urls): url for url in next_level_urls}
